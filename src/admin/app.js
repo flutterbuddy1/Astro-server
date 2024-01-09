0124843ed5229a2a0c2d83e0267289c31f4315d5
flutterbuddy1 <mayanksmind@gmail.com>
2024-01-09 21:39:57 +0530

    first commit

---
+++ src/admin/app.js
@@ -0,0 +1,17 @@
+const config = {
+  translations: {
+    en: {
+      'app.components.LeftMenu.navbrand.title': 'Astrology Dashboard',
+    }
+  },
+
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
